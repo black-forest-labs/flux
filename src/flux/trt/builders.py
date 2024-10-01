407a3ac0ffc84c470dc456938665afacb8394490
Sandro Cavallari <scavallari@nvidia.com>
2024-10-01 17:40:38 +0200

    implement load engines function

--- src/flux/trt/builders.py
+++ src/flux/trt/builders.py
@@ -1,3 +1,8 @@
+import torch
+import os
+import tensorrt as trt
+from typing import Any
+
 from flux.modules.autoencoder import AutoEncoder
 from flux.modules.conditioner import HFEmbedder
 from flux.model import Flux
@@ -24,65 +29,182 @@ class TRTBuilder:
             "ae": ae_model,
         }
 
+        assert all(stage in self.models for stage in self.stages)
 
-# def _prepare_model_configs(self, onnx_dir, engine_dir, enable_refit, int8, fp8, quantization_level, quantization_percentile, quantization_alpha, calibration_size):
-#     model_names = self.models.keys()
-#     lora_suffix = self._get_lora_suffix()
-#     self.torch_fallback = dict(zip(model_names, [self.torch_inference or self.config.get(model_name.replace('-','_')+'_torch_fallback', False) for model_name in model_names]))
+    @staticmethod
+    def _create_directories(engine_dir: str, onnx_dir: str):
+        # Create directories if missing
+        for directory in [engine_dir, onnx_dir]:
+            print(f"[I] Create directory: {directory} if not existing")
+            os.makedirs(directory, exist_ok=True)
 
-#     configs = {}
-#     for model_name in model_names:
-#         config = {
-#             'do_engine_refit': not self.pipeline_type.is_sd_xl_refiner() and enable_refit and model_name.startswith('unet'),
-#             'do_lora_merge': not enable_refit and self.lora_loader and model_name.startswith('unet'),
-#             'use_int8': False,
-#             'use_fp8': False,
-#         }
-#         config['model_suffix'] = lora_suffix if config['do_lora_merge'] else ''
+    @staticmethod
+    def _get_onnx_path(
+        model_name: str,
+        onnx_dir: str,
+        opt: bool = True,
+        suffix: str = "",
+    ) -> str:
+        onnx_model_dir = os.path.join(
+            onnx_dir,
+            model_name + suffix + (".opt" if opt else ""),
+        )
+        os.makedirs(onnx_model_dir, exist_ok=True)
+        return os.path.join(onnx_model_dir, "model.onnx")
 
-#         if int8:
-#             assert self.pipeline_type.is_sd_xl_base() or self.version in ["1.5", "2.1", "2.1-base"], "int8 quantization only supported for SDXL, SD1.5 and SD2.1 pipeline"
-#             if model_name == ('unetxl' if self.pipeline_type.is_sd_xl() else 'unet'):
-#                 config['use_int8'] = True
-#                 config['model_suffix'] += f"-int8.l{quantization_level}.bs2.s{self.denoising_steps}.c{calibration_size}.p{quantization_percentile}.a{quantization_alpha}"
-#         elif fp8:
-#             assert self.pipeline_type.is_sd_xl() or self.version in ["1.5", "2.1", "2.1-base"], "fp8 quantization only supported for SDXL, SD1.5 and SD2.1 pipeline"
-#             if model_name == ('unetxl' if self.pipeline_type.is_sd_xl() else 'unet'):
-#                 config['use_fp8'] = True
-#                 config['model_suffix'] += f"-fp8.l{quantization_level}.bs2.s{self.denoising_steps}.c{calibration_size}.p{quantization_percentile}.a{quantization_alpha}"
+    @staticmethod
+    def _get_engine_path(
+        model_name: str,
+        engine_dir: str,
+        suffix: str = "",
+    ) -> str:
+        return os.path.join(
+            engine_dir,
+            model_name + suffix + ".trt" + trt.__version__ + ".plan",
+        )
 
-#         config['onnx_path'] = self._get_onnx_path(model_name, onnx_dir, opt=False, suffix=config['model_suffix'])
-#         config['onnx_opt_path'] = self._get_onnx_path(model_name, onnx_dir, suffix=config['model_suffix'])
-#         config['engine_path'] = self._get_engine_path(model_name, engine_dir, config['do_engine_refit'], suffix=config['model_suffix'])
-#         config['weights_map_path'] = self._get_weights_map_path(model_name, onnx_dir) if config['do_engine_refit'] else None
-#         config['state_dict_path'] = self._get_state_dict_path(model_name, onnx_dir, suffix=config['model_suffix'])
-#         config['refit_weights_path'] = self._get_refit_nodes_path(model_name, onnx_dir, suffix=lora_suffix)
+    @staticmethod
+    def _get_weights_map_path(
+        model_name: str,
+        onnx_dir: str,
+    ) -> str:
+        onnx_model_dir = os.path.join(onnx_dir, model_name + ".opt")
+        os.makedirs(onnx_model_dir, exist_ok=True)
+        return os.path.join(onnx_model_dir, "weights_map.json")
 
-#         configs[model_name] = config
+    @staticmethod
+    def _get_refit_nodes_path(
+        model_name: str,
+        onnx_dir: str,
+        suffix: str = "",
+    ) -> str:
+        onnx_model_dir = os.path.join(onnx_dir, model_name + ".opt")
+        os.makedirs(onnx_model_dir, exist_ok=True)
+        return os.path.join(onnx_model_dir, "refit" + suffix + ".json")
 
-#     return configs
+    @staticmethod
+    def _get_state_dict_path(
+        model_name: str,
+        onnx_dir: str,
+        suffix: str = "",
+    ) -> str:
+        onnx_model_dir = os.path.join(onnx_dir, model_name + suffix)
+        os.makedirs(onnx_model_dir, exist_ok=True)
+        return os.path.join(onnx_model_dir, "state_dict.pt")
 
+    def _prepare_model_configs(
+        self,
+        engine_dir: str,
+        onnx_dir: str,
+    ) -> dict[str, dict[str, Any]]:
+        model_names = self.models.keys()
+        configs = {}
+        for model_name in model_names:
+            config: dict[str, Any] = {
+                "use_int8": False,
+                "use_fp8": False,
+            }
+            config["model_suffix"] = ""
 
-# def load_engines(
-#     self,
-#     engine_dir,
-#     framework_model_dir,
-#     onnx_dir,
-#     onnx_opset,
-#     opt_batch_size,
-#     opt_image_height,
-#     opt_image_width,
-#     optimization_level=3,
-#     static_batch=False,
-#     static_shape=True,
-#     enable_refit=False,
-#     enable_all_tactics=False,
-#     timing_cache=None,
-#     int8=False,
-#     fp8=False,
-#     quantization_level=2.5,
-#     quantization_percentile=1.0,
-#     quantization_alpha=0.8,
-#     calibration_size=32,
-#     calib_batch_size=2,
-# ):
+            config["onnx_path"] = self._get_onnx_path(
+                model_name=model_name,
+                onnx_dir=onnx_dir,
+                opt=False,
+                suffix=config["model_suffix"],
+            )
+            config["onnx_opt_path"] = self._get_onnx_path(
+                model_name=model_name,
+                onnx_dir=onnx_dir,
+                suffix=config["model_suffix"],
+            )
+            config["engine_path"] = self._get_engine_path(
+                model_name=model_name,
+                engine_dir=engine_dir,
+                suffix=config["model_suffix"],
+            )
+            config["state_dict_path"] = self._get_state_dict_path(
+                model_name=model_name,
+                onnx_dir=onnx_dir,
+                suffix=config["model_suffix"],
+            )
+
+            configs[model_name] = config
+
+        return configs
+
+    def _export_onnx(
+        self,
+        obj: torch.Module,
+        model_config: dict[str, Any],
+        opt_image_height: int,
+        opt_image_width: int,
+        static_shape: bool,
+        onnx_opset: int,
+        quantization_level: float,
+        quantization_percentile: float,
+        quantization_alpha: float,
+        calibration_size: int,
+        calib_batch_size: int,
+    ):
+        do_export_onnx = not os.path.exists(model_config["engine_path"]) and not os.path.exists(
+            model_config["onnx_opt_path"]
+        )
+
+        if do_export_onnx:
+            obj.export_onnx(
+                model_config["onnx_path"],
+                model_config["onnx_opt_path"],
+                onnx_opset,
+                opt_image_height,
+                opt_image_width,
+                enable_lora_merge=model_config["do_lora_merge"],
+                static_shape=static_shape,
+            )
+
+    def load_engines(
+        self,
+        engine_dir: str,
+        onnx_dir: str,
+        onnx_opset: int,
+        opt_batch_size: int,
+        opt_image_height: int,
+        opt_image_width: int,
+        optimization_level=3,
+        static_batch=False,
+        static_shape=True,
+        enable_refit=False,
+        enable_all_tactics=False,
+        timing_cache=None,
+        int8=False,
+        fp8=False,
+        quantization_level=2.5,
+        quantization_percentile=1.0,
+        quantization_alpha=0.8,
+        calibration_size=32,
+        calib_batch_size=2,
+    ):
+        self._create_directories(
+            engine_dir=engine_dir,
+            onnx_dir=onnx_dir,
+        )
+
+        model_configs = self._prepare_model_configs(
+            engine_dir=engine_dir,
+            onnx_dir=onnx_dir,
+        )
+
+        # Export models to ONNX
+        for model_name, obj in self.models.items():
+            self._export_onnx(
+                obj,
+                model_config=model_configs[model_name],
+                opt_image_height=opt_image_height,
+                opt_image_width=opt_image_width,
+                static_shape=static_shape,
+                onnx_opset=onnx_opset,
+                quantization_level=quantization_level,
+                quantization_percentile=quantization_percentile,
+                quantization_alpha=quantization_alpha,
+                calibration_size=calibration_size,
+                calib_batch_size=calib_batch_size,
+            )
