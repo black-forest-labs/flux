89f616e3a3e9128890de6a44432b550fba8845c7
Sandro Cavallari <scavallari@nvidia.com>
2024-10-02 15:28:38 +0200

    removed custom configurations

--- src/flux/trt/wrappers/t5_wrapper.py
+++ src/flux/trt/wrappers/t5_wrapper.py
@@ -12,9 +12,6 @@ class T5Wrapper(BaseWrapper):
         bf16=False,
         max_batch=16,
         verbose=True,
-        output_hidden_states=False,
-        do_constant_folding=True,
-        keep_pooled_output=False,
     ):
         super().__init__(
             model=model,
@@ -24,16 +21,10 @@ class T5Wrapper(BaseWrapper):
             bf16=bf16,
             max_batch=max_batch,
             verbose=verbose,
-            do_constant_folding=do_constant_folding,
         )
 
         self.text_maxlen = self.model.max_length
-        self.keep_pooled_output = keep_pooled_output
         self.hidden_layer_offset = -1
-
-        # Output the final hidden state
-        if output_hidden_states:
-            self.extra_output_names = ["hidden_states"]
 
         # set proper dtype
         self.set_model_to_dtype()
@@ -43,8 +34,6 @@ class T5Wrapper(BaseWrapper):
 
     def get_output_names(self):
         output_names = ["text_embeddings"]
-        if self.keep_pooled_output:
-            output_names += ["pooled_embeddings"]
         return output_names
 
     def get_dynamic_axes(self):
@@ -52,8 +41,6 @@ class T5Wrapper(BaseWrapper):
             "input_ids": {0: "B"},
             "text_embeddings": {0: "B"},
         }
-        if self.keep_pooled_output:
-            dynamic_axes["pooled_embeddings"] = {0: "B"}
         return dynamic_axes
 
     def check_dims(
@@ -79,25 +66,3 @@ class T5Wrapper(BaseWrapper):
 
     def get_model(self) -> torch.nn.Module:
         return self.model.hf_module
-
-    def optimize(self, onnx_graph, return_onnx=True):
-        opt = Optimizer(onnx_graph, verbose=self.verbose)
-        opt.info(self.name + ": original")
-        keep_outputs = [0, 1] if self.keep_pooled_output else [0]
-        opt.select_outputs(keep_outputs)
-        opt.cleanup()
-        opt.fold_constants()
-        opt.info(self.name + ": fold constants")
-        opt.infer_shapes()
-        opt.info(self.name + ": shape inference")
-        opt.select_outputs(keep_outputs, names=self.get_output_names())  # rename network outputs
-        opt.info(self.name + ": rename network output(s)")
-        opt_onnx_graph = opt.cleanup(return_onnx=return_onnx)
-        if "hidden_states" in self.extra_output_names:
-            opt_onnx_graph = opt.clip_add_hidden_states(
-                hidden_layer_offset=self.hidden_layer_offset,
-                return_onnx=return_onnx,
-            )
-            opt.info(self.name + ": added hidden_states")
-        opt.info(self.name + ": finished")
-        return opt_onnx_graph
