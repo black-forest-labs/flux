a8af1d826d15a2556a6a45f76f7ce084a6898ef0
Sandro Cavallari <scavallari@nvidia.com>
2024-10-02 19:17:24 +0200

    add AEWrapper step

--- src/flux/trt/wrappers/__init__.py
+++ src/flux/trt/wrappers/__init__.py
@@ -1,10 +1,12 @@
 from .base_wrapper import BaseWrapper
+from .ae_wrapper import AEWrapper
 from .clip_wrapper import CLIPWrapper
 from .flux_wrapper import FluxWrapper
 from .t5_wrapper import T5Wrapper
 
 __all__ = [
     "BaseWrapper",
+    "AEWrapper",
     "CLIPWrapper",
     "FluxWrapper",
     "T5Wrapper",
