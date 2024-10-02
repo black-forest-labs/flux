ec7537242f82dd002bee7a946ce164b593a507a9
Sandro Cavallari <scavallari@nvidia.com>
2024-10-02 17:37:56 +0200

    use flux exporter

--- src/flux/trt/wrappers/__init__.py
+++ src/flux/trt/wrappers/__init__.py
@@ -1,5 +1,11 @@
 from .base_wrapper import BaseWrapper
 from .clip_wrapper import CLIPWrapper
+from .flux_wrapper import FluxWrapper
 from .t5_wrapper import T5Wrapper
 
-__all__ = ["BaseWrapper", "CLIPWrapper", "T5Wrapper",]
+__all__ = [
+    "BaseWrapper",
+    "CLIPWrapper",
+    "FluxWrapper",
+    "T5Wrapper",
+]
