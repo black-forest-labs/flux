ecf6c4f7ef0ce501889a17974aa3937070f24653
Sandro Cavallari <scavallari@nvidia.com>
2024-10-14 23:06:19 +0200

    from onnxwrapper to onnxengine

--- src/flux/trt/onnx_export/__init__.py
+++ src/flux/trt/onnx_export/__init__.py
@@ -1,8 +1,8 @@
-from src.flux.trt.onnx_export.base_exporter import BaseExporter
-from src.flux.trt.onnx_export.ae_wrapper import AEExporter
-from src.flux.trt.onnx_export.clip_wrapper import CLIPExporter
-from src.flux.trt.onnx_export.flux_wrapper import FluxExporter
-from src.flux.trt.onnx_export.t5_wrapper import T5Exporter
+from flux.trt.onnx_export.base_exporter import BaseExporter
+from flux.trt.onnx_export.ae_exporter import AEExporter
+from flux.trt.onnx_export.clip_exporter import CLIPExporter
+from flux.trt.onnx_export.flux_exporter import FluxExporter
+from flux.trt.onnx_export.t5_exporter import T5Exporter
 
 __all__ = [
     "BaseExporter",
