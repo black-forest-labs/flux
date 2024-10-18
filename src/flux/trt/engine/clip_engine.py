315dd9d6aa430ab8e7b84a5b6ce03210bd27a34d
Sandro Cavallari <scavallari@nvidia.com>
2024-10-18 19:47:06 +0200

    clip engine and exporter use common mixin for managing parameters

--- src/flux/trt/engine/clip_engine.py
+++ src/flux/trt/engine/clip_engine.py
@@ -3,19 +3,21 @@ from math import ceil
 import torch
 
 from flux.trt.engine.base_engine import BaseEngine
+from flux.trt.mixin.clip_mixin import CLIPMixin
 
 
-class CLIPEngine(BaseEngine):
+class CLIPEngine(CLIPMixin, BaseEngine):
     def __init__(
         self,
         engine_path: str,
-        text_max_len: int,
-        hidden_size: int
-
+        text_maxlen: int,
+        hidden_size: int,
     ):
-        super().__init__(engine_path)
-        self.text_max_len = text_max_len
-        self. hidden_size = hidden_size
+        super().__init__(
+            text_maxlen=text_maxlen,
+            hidden_size=hidden_size,
+            engine_path=engine_path,
+        )
 
     def __call__(
         self,
@@ -44,8 +46,7 @@ class CLIPEngine(BaseEngine):
         image_height: int,
         image_width: int,
     ) -> dict[str, tuple]:
-
         return {
-            "input_ids": (batch_size, self.text_max_len),
+            "input_ids": (batch_size, self.text_maxlen),
             "text_embeddings": (batch_size, self.hidden_size),
         }
