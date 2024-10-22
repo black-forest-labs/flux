b96fd96589493bd7b3dcfc9561276010d6556dc2
Sandro Cavallari <scavallari@nvidia.com>
2024-10-22 10:39:01 +0200

    add licence

--- src/flux/trt/engine/__init__.py
+++ src/flux/trt/engine/__init__.py
@@ -1,3 +1,19 @@
+#
+# SPDX-FileCopyrightText: Copyright (c) 1993-2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from flux.trt.engine.base_engine import BaseEngine
 from flux.trt.engine.ae_engine import AEEngine
 from flux.trt.engine.flux_engine import FluxEngine
@@ -7,7 +23,7 @@ from flux.trt.engine.clip_engine import CLIPEngine
 __all__ = [
     "BaseEngine",
     "AEEngine",
-    "FluxEngine,"
+    "FluxEngine",
     "T5Engine",
     "CLIPEngine"
 ]
--- src/flux/trt/onnx_export/ae_exporter.py
+++ src/flux/trt/onnx_export/ae_exporter.py
@@ -1,8 +1,24 @@
+#
+# SPDX-FileCopyrightText: Copyright (c) 1993-2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import torch
 from math import ceil
 from flux.modules.autoencoder import AutoEncoder
 from flux.trt.onnx_export.base_exporter import BaseExporter
-from flux.trt.mixin.ae_mixin import AEMixin
+from flux.trt.mixin import AEMixin
 
 
 class AEExporter(AEMixin, BaseExporter):
