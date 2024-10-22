6743bb7012431c4857299a1f102e8d33658c5eee
Sandro Cavallari <scavallari@nvidia.com>
2024-10-22 10:39:29 +0200

    add licence

--- src/flux/trt/onnx_export/t5_exporter.py
+++ src/flux/trt/onnx_export/t5_exporter.py
@@ -1,8 +1,24 @@
+#
+# SPDX-FileCopyrightText: Copyright (c) 1993-2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import torch
 
 from flux.modules.conditioner import HFEmbedder
 from flux.trt.onnx_export.base_exporter import BaseExporter, TransformersModelWrapper
-from flux.trt.mixin.t5_mixin import T5Mixin
+from flux.trt.mixin import T5Mixin
 
 
 class T5Exporter(T5Mixin, BaseExporter):
