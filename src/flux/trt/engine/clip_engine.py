820e13dbf1172f1e2c8841fa467bd48296cfb7e5
Sandro Cavallari <scavallari@nvidia.com>
2024-10-22 10:39:01 +0200

    add licence

--- src/flux/trt/engine/clip_engine.py
+++ src/flux/trt/engine/clip_engine.py
@@ -1,9 +1,23 @@
-from math import ceil
+#
+# SPDX-FileCopyrightText: Copyright (c) 1993-2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import torch
 
-from flux.trt.engine.base_engine import BaseEngine
-from flux.trt.mixin.clip_mixin import CLIPMixin
+from flux.trt.engine import BaseEngine
+from flux.trt.mixin import CLIPMixin
 from transformers import CLIPTokenizer
 
 
