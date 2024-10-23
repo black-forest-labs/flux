33bc0950bb382effbda162f90025bf44e96223cd
Sandro Cavallari <scavallari@nvidia.com>
2024-10-23 10:40:50 +0200

    remove `should_be_dtype` from t5

--- src/flux/trt/mixin/t5_mixin.py
+++ src/flux/trt/mixin/t5_mixin.py
@@ -16,7 +16,6 @@
 
 from typing import Any
 
-from torch import dtype
 from flux.trt.mixin.base_mixin import BaseMixin
 
 
@@ -25,19 +24,16 @@ class T5Mixin(BaseMixin):
         self,
         text_maxlen: int,
         hidden_size: int,
-        should_be_dtype: dtype,
         *args,
         **kwargs,
     ) -> None:
         super().__init__(*args, **kwargs)
         self.text_maxlen = text_maxlen
         self.hidden_size = hidden_size
-        self.should_be_dtype = should_be_dtype
 
     def get_mixin_params(self) -> dict[str, Any]:
         """helper class that return the parameters used for construction"""
         return {
             "text_maxlen": self.text_maxlen,
             "hidden_size": self.hidden_size,
-            "should_be_dtype": self.should_be_dtype,
         }
