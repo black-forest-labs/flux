d1e1a0c1bb95761975f5fb04755cf77a687f60d4
Sandro Cavallari <scavallari@nvidia.com>
2024-11-14 12:10:11 +0100

    remove unused import

--- src/flux/trt/mixin/base_mixin.py
+++ src/flux/trt/mixin/base_mixin.py
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from torch import dtype
 
 from abc import ABC, abstractmethod
 from typing import Any
