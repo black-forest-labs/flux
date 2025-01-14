5801579155365b15049c96b7f5f35f0d58e93115
Sandro Cavallari <scavallari@nvidia.com>
2025-01-14 11:31:32 +0000

    remove fp8 support

--- src/flux/trt/exporter/base_exporter.py
+++ src/flux/trt/exporter/base_exporter.py
@@ -15,7 +15,6 @@
 # limitations under the License.
 
 import os
-import re
 import tempfile
 from abc import ABC, abstractmethod
 from typing import Any
@@ -27,7 +26,6 @@ from transformers import PreTrainedModel
 import onnx
 import onnx_graphsurgeon as gs
 from onnx import shape_inference
-from onnxconverter_common.float16 import convert_float_to_float16
 from polygraphy.backend.onnx.loader import fold_constants
 
 from polygraphy.backend.trt import (
@@ -40,13 +38,6 @@ from polygraphy.backend.trt import (
 )
 from polygraphy.logger import G_LOGGER
 import tensorrt as trt
-
-from .utils_modelopt import (
-    convert_zp_fp8,
-    cast_resize_io,
-    convert_fp16_io,
-    cast_fp8_mha_io,
-)
 
 from flux.modules.conditioner import HFEmbedder
 from flux.model import Flux
@@ -176,87 +167,6 @@ class Optimizer:
         if return_onnx:
             return onnx_graph
 
-    def fuse_mha_qkv_int8_sq(self):
-        tensors = self.graph.tensors()
-        keys = tensors.keys()
-
-        # mha  : fuse QKV QDQ nodes
-        # mhca : fuse KV QDQ nodes
-        q_pat = (
-            "/down_blocks.\\d+/attentions.\\d+/transformer_blocks"
-            ".\\d+/attn\\d+/to_q/input_quantizer/DequantizeLinear_output_0"
-        )
-        k_pat = (
-            "/down_blocks.\\d+/attentions.\\d+/transformer_blocks"
-            ".\\d+/attn\\d+/to_k/input_quantizer/DequantizeLinear_output_0"
-        )
-        v_pat = (
-            "/down_blocks.\\d+/attentions.\\d+/transformer_blocks"
-            ".\\d+/attn\\d+/to_v/input_quantizer/DequantizeLinear_output_0"
-        )
-
-        qs = sorted(
-            (
-                x.group(0)
-                for x in filter(
-                    lambda x: x is not None,
-                    [re.match(q_pat, key) for key in keys],
-                )
-            )
-        )
-        ks = sorted(
-            (
-                x.group(0)
-                for x in filter(
-                    lambda x: x is not None,
-                    [re.match(k_pat, key) for key in keys],
-                )
-            )
-        )
-        vs = sorted(
-            (
-                x.group(0)
-                for x in filter(
-                    lambda x: x is not None,
-                    [re.match(v_pat, key) for key in keys],
-                )
-            )
-        )
-
-        removed = 0
-        assert len(qs) == len(ks) == len(vs), "Failed to collect tensors"
-        for q, k, v in zip(qs, ks, vs):
-            is_mha = all("attn1" in tensor for tensor in [q, k, v])
-            is_mhca = all("attn2" in tensor for tensor in [q, k, v])
-            assert (is_mha or is_mhca) and (not (is_mha and is_mhca))
-
-            if is_mha:
-                tensors[k].outputs[0].inputs[0] = tensors[q]
-                tensors[v].outputs[0].inputs[0] = tensors[q]
-                del tensors[k]
-                del tensors[v]
-                removed += 2
-            else:  # is_mhca
-                tensors[k].outputs[0].inputs[0] = tensors[v]
-                del tensors[k]
-                removed += 1
-        print(f"Removed {removed} QDQ nodes")
-        return removed  # expected 72 for L2.5
-
-    def modify_fp8_graph(self):
-        onnx_graph = gs.export_onnx(self.graph)
-        # Convert INT8 Zero to FP8.
-        onnx_graph = convert_zp_fp8(onnx_graph)
-        # Convert weights and activations to FP16 and insert Cast nodes in FP8 MHA.
-        onnx_graph = convert_float_to_float16(onnx_graph, keep_io_types=True, disable_shape_infer=True)
-        self.graph = gs.import_onnx(onnx_graph)
-        # Add cast nodes to Resize I/O.
-        cast_resize_io(self.graph)
-        # Convert model inputs and outputs to fp16 I/O.
-        convert_fp16_io(self.graph)
-        # Add cast nodes to MHA's BMM1 and BMM2's I/O.
-        cast_fp8_mha_io(self.graph)
-
 
 class BaseExporter(ABC):
     def __init__(
@@ -319,6 +229,7 @@ class BaseExporter(ABC):
         image_height: int,
         image_width: int,
         static_batch: bool,
+        static_shape: bool,
     ) -> dict[str, Any]:
         pass
 
@@ -331,7 +242,7 @@ class BaseExporter(ABC):
         pass
 
     @abstractmethod
-    def check_dims(self, *args) -> None | tuple[int, int]:
+    def check_dims(self, *args, **kwargs) -> None | tuple[int, int]:
         pass
 
     # Helper utility for ONNX export
@@ -370,7 +281,7 @@ class BaseExporter(ABC):
                     )
 
                 # WAR: Enable autocast for BF16 Stable Cascade pipeline
-                with torch.inference_mode(), torch.autocast("cuda", enabled=False):
+                with torch.inference_mode():
                     export_onnx(self.get_model())
             else:
                 print(f"[I] Found cached ONNX model: {onnx_path}")
@@ -387,7 +298,6 @@ class BaseExporter(ABC):
                     convert_attribute=False,
                 )
             else:
-                print("debuglog - save the model unoptimized")
                 onnx.save(onnx_opt_graph, onnx_opt_path)
         else:
             print(f"[I] Found cached optimized ONNX model: {onnx_opt_path} ")
@@ -397,17 +307,12 @@ class BaseExporter(ABC):
         opt.info(self.name + ": original")
         opt.cleanup()
         opt.info(self.name + ": cleanup")
-        if kwargs.get("modify_fp8_graph", False):
-            opt.modify_fp8_graph()
-            opt.info(self.name + ": modify fp8 graph")
-        else:
-            opt.fold_constants()
-            opt.info(self.name + ": fold constants")
-            opt.infer_shapes()
-            opt.info(self.name + ": shape inference")
-            if kwargs.get("fuse_mha_qkv_int8", False):
-                opt.fuse_mha_qkv_int8_sq()
-                opt.info(self.name + ": fuse QKV nodes")
+
+        opt.fold_constants()
+        opt.info(self.name + ": fold constants")
+        opt.infer_shapes()
+        opt.info(self.name + ": shape inference")
+
         onnx_opt_graph = opt.cleanup(return_onnx=return_onnx)
         opt.info(self.name + ": finished")
         return onnx_opt_graph
