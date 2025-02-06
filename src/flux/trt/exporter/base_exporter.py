df21697476f34894603266e2bcd229dec2c0808d
Sandro Cavallari <scavallari@nvidia.com>
2025-02-06 14:41:09 +0000

    remove onnx related stuff and add TRTBaseConfig class + registry

--- src/flux/trt/exporter/base_exporter.py
+++ src/flux/trt/exporter/base_exporter.py
@@ -1,5 +1,5 @@
 #
-# SPDX-FileCopyrightText: Copyright (c) 1993-2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 1993-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,207 +16,85 @@
 
 import os
 import subprocess
-import tempfile
 from abc import ABC, abstractmethod
 from collections import defaultdict
-from colored import fore, style
+from dataclasses import dataclass, field
 from typing import Any
 
-import onnx
-import onnx_graphsurgeon as gs
-import torch
-from onnx import shape_inference
-from polygraphy.backend.onnx.loader import fold_constants
-from torch import Tensor, nn
-from transformers import PreTrainedModel
+from tensorrt import __version__ as trt_version
+from colored import fore, style
 
-from flux.model import Flux
-from flux.modules.conditioner import HFEmbedder
+registry = {}
 
 
-class TransformersModelWrapper(torch.nn.Module):
-    def __init__(self, model: HFEmbedder, output_name: str):
-        super().__init__()
-        self.model: PreTrainedModel = model.hf_module
-        self.output_name = output_name
+@dataclass
+class TRTBaseConfig:
+    onnx_dir: str
+    engine_dir: str
+    precision: str
+    trt_verbose: bool
+    trt_static_batch: bool
+    trt_static_shape: bool
+    model_name: str
+    onnx_path: str = field(init=False)
+    engine_path: str = field(init=False)
+    trt_update_output_names: list[str] | None = None
+    trt_enable_all_tactics: bool = False
+    trt_timing_cache: str | None = None
+    trt_native_instancenorm: bool = True
+    trt_builder_optimization_level: int = 3
+    trt_precision_constraints: str = "none"
 
-    def forward(self, input_ids: Tensor, *args):
-        outputs = self.model.forward(
-            input_ids=input_ids,
-            attention_mask=None,
-            output_hidden_states=False,
+    def __post_init__(self):
+        self.onnx_path = self._get_onnx_path()
+        self.engine_path = self._get_engine_path()
+        assert os.path.isfile(self.onnx_path), "onnx_path do not exists: {}".format(self.onnx_path)
+
+    def _get_onnx_path(self) -> str:
+        onnx_model_dir = os.path.join(
+            self.onnx_dir,
+            self.model_name + ".opt",
         )
-        text_embeddings = outputs[self.output_name]
-        return text_embeddings
+        return os.path.join(onnx_model_dir, "model.onnx")
 
-
-class FluxModelWrapper(torch.nn.Module):
-    def __init__(self, model: Flux):
-        super().__init__()
-        self.base_model = model
-
-    def forward(
-        self,
-        hidden_states: Tensor,
-        encoder_hidden_states: Tensor,
-        pooled_projections: Tensor,
-        timestep: Tensor,
-        img_ids: Tensor,
-        txt_ids: Tensor,
-        guidance: Tensor | None = None,
-    ) -> Tensor:
-        # add batch dim to img_ids and txt_ids
-        img_ids = torch.unsqueeze(img_ids, 0)
-        txt_ids = torch.unsqueeze(txt_ids, 0)
-
-        return self.base_model(
-            img=hidden_states,
-            img_ids=img_ids,
-            txt=encoder_hidden_states,
-            txt_ids=txt_ids,
-            timesteps=timestep,
-            y=pooled_projections,
-            guidance=guidance,
+    def _get_engine_path(self) -> str:
+        return os.path.join(
+            self.engine_dir,
+            self.model_name + ".trt" + trt_version + ".plan",
         )
 
+def register_config(model_name: str, tf32=True, bf16=False, fp8=False, fp4=False, t5_fp8=False):
+    """Decorator to register a configuration class with specific flag conditions."""
 
-class Optimizer:
-    def __init__(self, onnx_graph, verbose=False):
-        self.graph = gs.import_onnx(onnx_graph)
-        self.verbose = verbose
-
-    def info(self, prefix):
-        if self.verbose:
-            print(
-                f"{prefix} .. {len(self.graph.nodes)} nodes, {len(self.graph.tensors().keys())} tensors, {len(self.graph.inputs)} inputs, {len(self.graph.outputs)} outputs"
-            )
-
-    def cleanup(self, return_onnx=False):
-        self.graph.cleanup().toposort()
-        return gs.export_onnx(self.graph) if return_onnx else self.graph
-
-    def select_outputs(self, keep, names=None):
-        self.graph.outputs = [self.graph.outputs[o] for o in keep]
-        if names:
-            for i, name in enumerate(names):
-                self.graph.outputs[i].name = name
-
-    def fold_constants(self, return_onnx=False):
-        onnx_graph = fold_constants(
-            gs.export_onnx(self.graph),
-            allow_onnxruntime_shape_inference=True,
-            error_ok=False,
-        )
-        self.graph = gs.import_onnx(onnx_graph)
-        if return_onnx:
-            return onnx_graph
-
-    def infer_shapes(self, return_onnx=False):
-        onnx_graph = gs.export_onnx(self.graph)
-        if onnx_graph.ByteSize() > 2147483648:
-            temp_dir = tempfile.TemporaryDirectory().name
-            os.makedirs(temp_dir, exist_ok=True)
-            onnx_orig_path = os.path.join(temp_dir, "model.onnx")
-            onnx_inferred_path = os.path.join(temp_dir, "inferred.onnx")
-            onnx.save_model(
-                onnx_graph,
-                onnx_orig_path,
-                save_as_external_data=True,
-                all_tensors_to_one_file=True,
-                convert_attribute=False,
-            )
-            onnx.shape_inference.infer_shapes_path(
-                model_path=onnx_orig_path,
-                output_path=onnx_inferred_path,
-            )
-            onnx_graph = onnx.load(onnx_inferred_path)
+    def decorator(cls):
+        if model_name == "t5":
+            key = f"model={model_name}_tf32={tf32}_bf16={bf16}_fp8={fp8}_fp4={fp4}_t5-fp8={t5_fp8}"
         else:
-            onnx_graph = shape_inference.infer_shapes(onnx_graph)
+            key = f"model={model_name}_tf32={tf32}_bf16={bf16}_fp8={fp8}_fp4={fp4}"
+        registry[key] = cls
+        return cls
 
-        self.graph = gs.import_onnx(onnx_graph)
-        if return_onnx:
-            return onnx_graph
+    return decorator
 
-    def clip_add_hidden_states(self, hidden_layer_offset, return_onnx=False):
-        hidden_layers = -1
-        onnx_graph = gs.export_onnx(self.graph)
-        for i in range(len(onnx_graph.graph.node)):
-            for j in range(len(onnx_graph.graph.node[i].output)):
-                name = onnx_graph.graph.node[i].output[j]
-                if "layers" in name:
-                    hidden_layers = max(int(name.split(".")[1].split("/")[0]), hidden_layers)
-        for i in range(len(onnx_graph.graph.node)):
-            for j in range(len(onnx_graph.graph.node[i].output)):
-                if onnx_graph.graph.node[i].output[
-                    j
-                ] == "/text_model/encoder/layers.{}/Add_1_output_0".format(
-                    hidden_layers + hidden_layer_offset
-                ):
-                    onnx_graph.graph.node[i].output[j] = "hidden_states"
-            for j in range(len(onnx_graph.graph.node[i].input)):
-                if onnx_graph.graph.node[i].input[j] == "/text_model/encoder/layers.{}/Add_1_output_0".format(
-                    hidden_layers + hidden_layer_offset
-                ):
-                    onnx_graph.graph.node[i].input[j] = "hidden_states"
-        if return_onnx:
-            return onnx_graph
+
+def get_config(model_name: str, tf32=True, bf16=True, fp8=False, fp4=False, t5_fp8=False):
+    """Retrieve the appropriate configuration instance based on current flags."""
+    if model_name == "t5":
+        key = f"model={model_name}_tf32={tf32}_bf16={bf16}_fp8={fp8}_fp4={fp4}_t5-fp8={t5_fp8}"
+    else:
+        key = f"model={model_name}_tf32={tf32}_bf16={bf16}_fp8={fp8}_fp4={fp4}"
+    return registry[key]
 
 
 class BaseExporter(ABC):
     def __init__(
         self,
-        model: nn.Module,
-        tf32=False,
-        bf16=False,
-        fp8=False,
-        fp4=False,
+        trt_config: TRTBaseConfig,
         max_batch=4,
-        verbose=False,
-        do_constant_folding=True,
-        build_strongly_typed=False,
     ):
-        self.model = model
-        self.name = model.__class__.__name__
-        self.verbose = verbose
-        self.do_constant_folding = do_constant_folding
-        self.build_strongly_typed = build_strongly_typed
-
-        self.tf32 = tf32
-        self.bf16 = bf16
-        self.fp8 = fp8
-        self.fp4 = fp4
-
         self.min_batch = 1
         self.max_batch = max_batch
-        self.extra_output_names = []
-
-    @property
-    def device(self):
-        return next(self.model.parameters()).device
-
-    def prepare_model(self):
-        if self.bf16 or self.fp8 or self.fp4:
-            self.model = self.model.to(dtype=torch.bfloat16)
-        else:
-            self.model = self.model.to(dtype=torch.float32)
-
-        self.model = self.model.eval().requires_grad_(False)
-
-    def get_model(self) -> torch.nn.Module:
-        return self.model
-
-    @abstractmethod
-    def get_sample_input(
-        self,
-        batch_size: int,
-        opt_image_height: int,
-        opt_image_width: int,
-    ) -> tuple | torch.Tensor:
-        pass
-
-    @abstractmethod
-    def get_input_names(self) -> list[str]:
-        pass
+        self.trt_config = trt_config
 
     @abstractmethod
     def get_input_profile(
@@ -230,88 +108,8 @@ class BaseExporter(ABC):
         pass
 
     @abstractmethod
-    def get_output_names(self) -> list[str]:
-        pass
-
-    @abstractmethod
-    def get_dynamic_axes(self) -> dict[str, Any]:
-        pass
-
-    @abstractmethod
     def check_dims(self, *args, **kwargs) -> None | tuple[int, int]:
         pass
-
-    # Helper utility for ONNX export
-    def export_onnx(
-        self,
-        onnx_path: str,
-        onnx_opt_path: str,
-        onnx_opset: int,
-        opt_image_height: int,
-        opt_image_width: int,
-    ):
-        onnx_opt_graph = None
-        # Export optimized ONNX model (if missing)
-        if not os.path.exists(onnx_opt_path):
-            if not os.path.exists(onnx_path):
-                print(f"[I] Exporting ONNX model: {onnx_path}")
-                print("[I] model dtype: {}".format(next(self.model.parameters()).dtype))
-                print(f"[I] tf32 = {self.tf32} | bf16 = {self.bf16}")
-
-                def export_onnx(model: torch.nn.Module):
-                    inputs = self.get_sample_input(
-                        self.min_batch,
-                        opt_image_height,
-                        opt_image_width,
-                    )
-                    torch.onnx.export(
-                        model,
-                        inputs,
-                        onnx_path,
-                        export_params=True,
-                        opset_version=onnx_opset,
-                        do_constant_folding=self.do_constant_folding,
-                        input_names=self.get_input_names(),
-                        output_names=self.get_output_names(),
-                        dynamic_axes=self.get_dynamic_axes(),
-                    )
-
-                # WAR: Enable autocast for BF16 Stable Cascade pipeline
-                with torch.inference_mode():
-                    export_onnx(self.get_model())
-            else:
-                print(f"[I] Found cached ONNX model: {onnx_path}")
-
-            print(f"[I] Optimizing ONNX model: {onnx_opt_path}")
-            print("debuglog - loading the model to optimize")
-            onnx_opt_graph = self.optimize(onnx.load(onnx_path))
-            if onnx_opt_graph.ByteSize() > 2147483648:
-                onnx.save_model(
-                    onnx_opt_graph,
-                    onnx_opt_path,
-                    save_as_external_data=True,
-                    all_tensors_to_one_file=True,
-                    convert_attribute=False,
-                )
-            else:
-                onnx.save(onnx_opt_graph, onnx_opt_path)
-        else:
-            print(f"[I] Found cached optimized ONNX model: {onnx_opt_path} ")
-
-    def optimize(self, onnx_graph, return_onnx=True, *args, **kwargs):
-        opt = Optimizer(onnx_graph, verbose=self.verbose)
-        opt.info(self.name + ": original")
-        opt.cleanup()
-        opt.info(self.name + ": cleanup")
-
-        opt.fold_constants()
-        opt.info(self.name + ": fold constants")
-        opt.infer_shapes()
-        opt.info(self.name + ": shape inference")
-
-        onnx_opt_graph = opt.cleanup(return_onnx=return_onnx)
-        opt.info(self.name + ": finished")
-        return onnx_opt_graph
 
     @staticmethod
     def build(
@@ -323,10 +121,10 @@ class BaseExporter(ABC):
         fp8=False,
         fp4=False,
         input_profile: dict[str, Any] | None = None,
+        update_output_names: list[str] | None = None,
         enable_refit=False,
         enable_all_tactics=False,
         timing_cache=None,
-        update_output_names: list[str] | None = None,
         native_instancenorm=True,
         builder_optimization_level=3,
         precision_constraints="none",
