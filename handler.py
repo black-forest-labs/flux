57ef17a9e342c51b070e3014bc0885a049ff0281
AlexBaidin <labitase@gmail.com>
2025-05-07 20:59:11 +0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,6 +1,8 @@
-from demo_api import run_inference  # твоя функция генерации изображений
+import runpod
+from demo_api import run_inference  # Убедись, что demo_api.py существует и содержит функцию run_inference
 
-def handler(event):
-    prompt = event.get('input', {}).get('prompt', 'A cat in space')
+def handler(job):
+    prompt = job.get('input', {}).get('prompt', 'A cat in space')
     return run_inference(prompt)
 
+runpod.serverless.start({"handler": handler})
