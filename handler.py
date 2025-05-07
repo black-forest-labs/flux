a371285dfbefe478a6a5710d52eb12010654da53
AlexBaidin <labitase@gmail.com>
2025-05-07 20:36:46 +0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,7 +1,6 @@
-from runpod.serverless.utils.rp_validator import validate
-from runpod.serverless.modules.rp_handler import RunPodHandler
-from demo_api import run_inference  # предполагаем, что ты используешь эту функцию
+from demo_api import run_inference  # твоя функция генерации изображений
 
 def handler(event):
     prompt = event.get('input', {}).get('prompt', 'A cat in space')
     return run_inference(prompt)
+
